1d6046de0081f98a86c9cc8ed4f37663935976e7
md-liton <mdliton5506@gmail.com>
2024-09-26 23:55:00 +0600

    iglamours

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from '../assets/Logo3.png'
+import { HiSearch } from "react-icons/hi";
+import { BsCart4 } from "react-icons/bs";
+
+
+
+const Navbar = () => {
+  return (
+    <nav className='bg-secondry_color'>
+        <div className="container">
+            <div className="nav_main">
+                <div className="nav_top flex justify-between items-center">
+                    <div className='w-1/3'>
+                    <img src={logo} alt="logo" className=' h-[100px] cursor-pointer'/>
+                    </div>
+                    <div className='w-1/3 relative text-white'>
+                    <input type="text" placeholder='Search your item' className='focus:outline-none border-[2px] border-primary_color rounded-full w-full py-[10px] pl-[20px] pr-[40px] h-[50px] bg-transparent'  />
+                    <div className=' border-l-[2px] border-primary_color absolute top-0 right-[10px]  h-[50px] flex justify-center items-center p-[5px] text-[20px] cursor-pointer'>
+                    <HiSearch />
+                    </div>
+                    </div>
+                    <div className='w-1/3 flex justify-end items-center gap-[15px]'>
+                    <div className=' py-[5px] px-[10px] rounded-lg text-paragraph font-semibold cursor-pointer border-[2px] border-primary_color hover:bg-primary_color duration-300 text-white '>Login</div>
+                    <div className=' py-[5px] px-[10px] rounded-lg text-paragraph font-semibold cursor-pointer border-[2px] border-primary_color hover:bg-transparent duration-300 text-white flex items-center gap-[10px] bg-primary_color'> <BsCart4 /> Cart</div>
+                    </div>
+                </div>
+                <div className="nav_bottom"></div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+            xl: '1240px',
+            '2xl': '1400px'
+        }
+    },
+    screens: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+    },
+    fontFamily: {
+      'heading': ["Open Sans", "sans-serif"],
+      'paragraph': ["Edu AU VIC WA NT Guides", "cursive"]
+    },
+    colors: {
+      'primary_color': '#EC018B',
+      'secondry_color': '#192038',
+    },
+    },
+  },
+  plugins: [],
+}
